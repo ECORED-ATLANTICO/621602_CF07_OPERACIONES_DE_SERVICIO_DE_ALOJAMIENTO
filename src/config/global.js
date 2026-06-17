2f89f731d5306ed00bbffe48e55ed483a85a1970
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2026-06-16 22:56:43 -0500

    Agregado CFA

--- src/config/global.js
+++ src/config/global.js
@@ -189,7 +189,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/621602_CF7_DU.zip',
       },
       {
         icono: 'fas fa-download',
